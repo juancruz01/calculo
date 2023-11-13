13b42960be179e457b2d08b27e99faf9d57c0a23
juan cruz carisimo <juancruzc278@gmail.com>
2023-11-13 19:20:12 -0300

    ejemplo subidos

--- Program.cs
+++ Program.cs
@@ -24,14 +24,13 @@ bool esMayorADiez = superficieRectangulo > 10;
 if (esMayorADiez)
 {
     //mostrar el mensaje solo si es mayor a diez
-    Console.WriteLine("La superficie del rectangulo es mayor a 10");
+    Console.WriteLine("La superficie del rectangulo es mayor.");
 
 } else
 {
     //mostrar el mensaje solo si es menor a diez
-    Console.WriteLine("La superficie del rectangulo es menor");
+    Console.WriteLine("La superficie del rectangulo es menor.");
 
 }
-
 
 Console.ReadKey();
